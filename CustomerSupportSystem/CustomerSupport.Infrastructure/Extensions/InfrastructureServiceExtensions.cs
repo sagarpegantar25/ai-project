a5ef4fc4b89b0c7fa5c3578e8ff293f14fb433f4
AKGROUP\sagar.pegantar <sagar.pegantar@akgroup.co.in>
2026-09-21 14:19:18 +0530

    done

---
+++ CustomerSupportSystem/CustomerSupport.Infrastructure/Extensions/InfrastructureServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using CustomerSupport.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+namespace CustomerSupport.Infrastructure.Extensions;
+public static class InfrastructureServiceExtensions
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+                                                               string connectionString)
+    {
+        services.AddDbContext<CustomerSupportDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+
+        return services;
+    }
+}
